4d9d6d35210b331feb9a1bbeabe3562268fef006
Sven Anders Robbestad <robbestad@gmail.com>
2014-10-27 20:22:01 +0100

    first commit

---
+++ dist/pages/home.php
@@ -0,0 +1,110 @@
+<header class="topbar">
+    <nav class="width">
+        <a href="http://hhvm.com/"><h1>HHVM</h1></a>
+
+        <div class="menu-top-menu-container">
+            <ul id="menu-top-menu" class="menu">
+                <li id="menu-item-119" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-119"><a
+                        href="http://hhvm.com/blog">Blog</a></li>
+                <li id="menu-item-89" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-89"><a
+                        href="http://docs.hhvm.com">Docs</a></li>
+                <li id="menu-item-95" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-95"><a
+                        href="http://github.com/facebook/hhvm">Github</a></li>
+                <li id="menu-item-215" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-215"><a
+                        href="http://hacklang.org">Hack</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<header class="hero">
+    <div class="width">
+        <hgroup>
+            <h1>
+                Moving fast with high performance Hack and PHP </h1>
+            <a class="button" href="http://hhvm.com/manual/en/install.php">Download HHVM Now</a>
+        </hgroup>
+        <aside>
+            sss
+        </aside>
+    </div>
+</header>
+
+<section class="content">
+    <div class="width">
+        <article>
+            <article class="post-65 page type-page status-publish hentry article">
+                <h2><a href="http://hhvm.com/" rel="bookmark" title="Permanent Link to What is HHVM?">What is HHVM?</a>
+                </h2>
+
+                <p>HHVM is an <a href="http://github.com/facebook/hhvm">open-source</a> virtual machine designed for
+                    executing programs written in <a href="http://hacklang.org">Hack</a> and <a href="http://php.net">PHP</a>.
+                    HHVM uses a just-in-time (JIT) compilation approach to achieve superior performance while
+                    maintaining the development flexibility that PHP provides.</p>
+
+                <p>HHVM runs much of the world’s <a href="http://hhvm.com/frameworks">existing PHP</a>. <a
+                        href="http://hhvm.h4cc.de/">Developers</a> and <a
+                        href="http://www.hhvm.com/blog/1379/hhvm-on-heroku">hosts</a>&nbsp;are adopting HHVM.&nbsp;We
+                    are aware of&nbsp;minor incompatibilities (please <a
+                        href="https://github.com/facebook/hhvm/issues/new">open issues</a> when you find them), but&nbsp;we
+                    can <a href="http://hhvm.com/frameworks">run the top 20 Github PHP frameworks</a> out of the box.&nbsp;The
+                    HHVM team, along with many wonderful community members, has made it a <a
+                        href="http://www.hhvm.com/blog/3743/hhvm-the-next-six-months">stated, high priority goal</a> to
+                    run all existing PHP code existing out in the wild.</p>
+
+                <div class="content-column one_half"><p><strong>HHVM Features</strong></p>
+                    <ul>
+                        <li>The&nbsp;<a href="http://hacklang.org">Hack Language</a></li>
+                        <li><a href="http://www.hhvm.com/blog/2027/faster-and-cheaper-the-evolution-of-the-hhvm-jit">JIT
+                                Compilation</a></li>
+                        <li><a href="https://github.com/facebook/hhvm/wiki/Extension-API">HNI</a></li>
+                        <li><a href="http://www.hhvm.com/blog/1817/fastercgi-with-hhvm">FastCGI support</a></li>
+                        <li><a href="http://hhvm.com/frameworks">Increasing PHP5 Parity</a></li>
+                        <li><code>hphpd</code> debugger</li>
+                        <li>…. and <a href="http://docs.hhvm.com">more</a></li>
+                    </ul>
+                </div>
+                <div class="content-column one_half last_column"><p><strong>The JIT Compiler</strong></p>
+
+                    <p>Rather than directly interpret or <a
+                            href="https://en.wikipedia.org/wiki/HipHop_for_PHP#History_Before_HHVM">compile PHP code
+                            directly to C++</a>, HHVM compiles Hack and PHP into an intermediate bytecode. This bytecode
+                        is then translated into&nbsp;<a title="X64" href="https://en.wikipedia.org/wiki/X64">x64</a>&nbsp;machine
+                        code dynamically at runtime by a just-in-time (<a title="Just-in-time compilation"
+                                                                          href="https://en.wikipedia.org/wiki/Just-in-time_compilation">JIT</a>)
+                        compiler. This compilation process allows for all sorts of optimizations that cannot be made in
+                        a statically compiled binary, thus enabling higher performance of your Hack and PHP programs.
+                    </p></div>
+                <div class="clear_column"></div>
+
+            </article>
+
+            <hr>
+            <br>
+        </article>
+
+        <aside>
+            <h3>Community</h3>
+
+            <div class="menu-compatibility-container">
+                <ul id="menu-compatibility" class="menu">
+                    <li id="menu-item-4235"
+                        class="menu-item menu-item-type-custom menu-item-object-custom">
+                        <a href="http://hhvm.com/frameworks/">OSS Frameworks</a>
+                    </li>
+                </ul>
+                <div>
+                    <h3>Compatibility</h3>
+
+                    <div class="menu-compatibility-container">
+                        <ul id="menu-compatibility" class="menu">
+                            <li id="menu-item-4235"
+                                class="menu-item menu-item-type-custom menu-item-object-custom">
+                                <a href="http://hhvm.com/frameworks/">OSS Frameworks</a>
+                            </li>
+                        </ul>
+        </aside>
+
+    </div>
+</section>
+
