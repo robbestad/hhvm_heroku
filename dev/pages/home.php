4884ee85288691e9128bd80e90ee8c4a3b40016a
Sven Anders Robbestad <robbestad@gmail.com>
2014-11-17 12:09:41 +0100

    bower

--- dev/pages/home.php
+++ dev/pages/home.php
@@ -5,13 +5,9 @@
         <div class="menu-top-menu-container">
             <ul id="menu-top-menu" class="menu">
                 <li id="menu-item-119" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-119"><a
-                        href="http://hhvm.com/blog">Blog</a></li>
-                <li id="menu-item-89" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-89"><a
-                        href="http://docs.hhvm.com">Docs</a></li>
+                        href="http://www.robbestad.com">Blog</a></li>
                 <li id="menu-item-95" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-95"><a
-                        href="http://github.com/facebook/hhvm">Github</a></li>
-                <li id="menu-item-215" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-215"><a
-                        href="http://hacklang.org">Hack</a></li>
+                        href="https://github.com/svenanders/hhvm_heroku">Github</a></li>
             </ul>
         </div>
     </nav>
@@ -23,9 +19,19 @@
         <article>
             <article class="page">
                 <h2><a href="https://fierce-lake-5417.herokuapp.com/" rel="bookmark" 
-                title="Permanent Link to What is HHVM?">HHVM on Heroku</a>
+                title="HHVM on Heroku">HHVM on Heroku</a>
                 </h2>
-
+                <p>
+                This is a demo repo showing how you can get
+                up and running on HHVM with Heroku. <a href="http://www.robbestad.com/2014/11/adventures-in-hiphop-vm.html"
+                >Read this blog article</a> detailing how you get it running.
+                </p>
+            
+                <?php
+                if (defined('HHVM_VERSION')) {
+                        echo "<p>This page is running on HHVM ", HHVM_VERSION, " </p>\n";
+                    }
+                ?>
                 <div class="clear_column"></div>
 
             </article>
